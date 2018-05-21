962249172d62af2afb8a7873ec930fd834ea25ae
Paul <rpmstevenson@gmail.com>
2018-05-21 09:35:54 +0100

    update

---
+++ PDA_Static_and_Dynamic_Task_A/spec/card_spec.rb
@@ -0,0 +1,23 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative('../testing_task_2')
+require_relative('../card')
+
+class CardGameTest < MiniTest::Test
+
+  def test_check_for_ace
+    assert_equal(1,1)
+  end
+
+  # def test_highest_card()
+  #   card1 > card2
+  #   assert_equal(card1, card.value)
+  # end
+
+  def test_cards_total
+    
+    assert_equal()
+
+  end
+
+end
