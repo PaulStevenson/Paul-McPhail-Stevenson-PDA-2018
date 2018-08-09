e83984e355371dc265a9dcc26bb18ccd5d16218c
Paul <rpmstevenson@gmail.com>
2018-08-09 15:59:07 +0100

    Task B done

--- Task B/js_calculator_start_point/tests/integration/tests.js
+++ Task B/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,95 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should be able to add two single digit numbers', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number2')).click();
+      element(by.css('#operator_add')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  it('should be able to add two double digit numbers', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number2')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_add')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('44')
+  })
+
+  it('should be able to subract two single digit numbers', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number4')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should be able to multiply two single digit numbers', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number5')).click();
+      element(by.css('#operator_multiply')).click();
+      element(by.css('#number3')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('15')
+  })
+
+  it('should be able to divide two single digit numbers', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number8')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number4')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should be able to chain a caluculation', function () {
+      element(by.css('#number4')).click();
+      element(by.css('#operator_add')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number1')).click();
+      element(by.css('#operator_multiply')).click();
+      element(by.css('#number7')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('21')
+  })
+
+  it('should be able to show total as a decimal', function () {
+      element(by.css('#number1')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number1')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_equals')).click();
+
+      expect(running_total.getAttribute('value')).to.eventually.equal('0.1')
+  })
+
+  it('should be able to show total as a negative', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number1')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number5')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('-4')
+  })
+
+  it('should be able to divide by 0', function () {
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number8')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('Infinity')
+  })
+
+
+
 });
--- Task B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Task B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,6 +9,62 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
+})
+
+  it('can add numbers test', function () {
+       calculator.previousTotal = 3;
+      calculator.add(3);
+      
+      assert.equal(calculator.runningTotal, 6 )
+  })
+
+  it('can subract numbers test', function () {
+       calculator.previousTotal = 5;
+      calculator.subtract(4);
+
+      assert.equal(calculator.runningTotal,  1)
+  })
+
+  it('can multiply numbers test', function () {
+       calculator.previousTotal = 5;
+      calculator.multiply(4);
+
+      assert.equal(calculator.runningTotal,  20)
+  })
+
+  it('can divide numbers test', function () {
+       calculator.previousTotal = 20;
+      calculator.divide(4);
+
+      assert.equal(calculator.runningTotal,  5)
+  })
+
+  // - calculator.numberClick()
+  it("can click a number", function () {
+      calculator.numberClick(5);
+      calculator.numberClick(5);
+
+      assert.equal(calculator.runningTotal, 55)
+  })
+
+  // - calculator.operatorClick()
+  it('can do operator click', function () {
+      calculator.runningTotal = 5;
+      calculator.previousTotal = 5;
+      calculator.operatorClick('+');
+      calculator.operatorClick('=');
+
+      assert.equal(calculator.runningTotal, 10)
+
+  })
+
+  // - calculator.clearClick()
+  it('can clear the calculator', function () {
+      calculator.previousTotal = 3;
+     calculator.add(3);
+     calculator.clearClick()
+
+     assert.equal(calculator.runningTotal, 0)
   })
 
 });
